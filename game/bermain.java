5b005ad6ad98e6ffefa6c91c277a0193382c0bed
gina rahmadiani <ginnarahmadiani01@gmail.com>
2018-04-24 20:08:34 +0700

    Awal

---
+++ game/bermain.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bermain here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bermain extends World
+{
+
+    /**
+     * Constructor for objects of class bermain.
+     * 
+     */
+    public bermain()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 400, 1); 
+        prepare();
+    }  
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        gin gin = new gin();
+        addObject(gin,753,362);
+        mang mang = new mang();
+        addObject(mang,776,380);
+        gin gin2 = new gin();
+        addObject(gin2,363,390);
+        mang mang2 = new mang();
+        addObject(mang2,423,398);
+        gin gin3 = new gin();
+        addObject(gin3,20,389);
+        mang mang3 = new mang();
+        addObject(mang3,50,388);
+    }
+}
---
+++ game/gin.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class gin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class gin extends Actor
+{
+    /**
+     * Act - do whatever the gin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        setLocation(getY(),getY()-2);
+    }    
+}
