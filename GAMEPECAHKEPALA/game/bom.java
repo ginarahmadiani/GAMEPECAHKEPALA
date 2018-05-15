f9000c506b9cad5cbde17f163aa19f78cc87e805
ginarahmadiani <38679898+ginarahmadiani@users.noreply.github.com>
2018-05-15 09:03:47 +0700

    Add files via upload

---
+++ GAMEPECAHKEPALA/game/bom.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bom here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bom extends Actor
+{
+    /**
+     * Act - do whatever the bom wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public void act() 
+    {
+        // Add your action code here.
+        World w = getWorld();
+        setLocation(getX(),getY()-1);
+        if(isAtEdge())
+        {
+            //w.removeObject(this);
+            setLocation(590,getX());
+        }
+    }    
+}
+
---
+++ GAMEPECAHKEPALA/game/buaya.java
@@ -0,0 +1,59 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class buaya here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class buaya extends Actor
+{
+    /**
+     * Act - do whatever the buaya wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public int currentRotation = 0;
+    public void act() 
+    {
+        KeyboardControls();
+        eatgin();
+        eatmang();
+        eatbom();
+        
+    }
+    public void KeyboardControls(){
+    if(Greenfoot.isKeyDown("up")){
+    move(5);
+    }
+    if(Greenfoot.isKeyDown("down")){
+    move(-5);
+    }
+    if(Greenfoot.isKeyDown("right")){
+    currentRotation+=2;
+    setRotation(currentRotation);
+    }
+    if(Greenfoot.isKeyDown("left")){
+    currentRotation-=2;
+    setRotation(currentRotation);
+    }
+    }
+    
+    public void eatgin(){
+        if(isTouching(gin.class)){
+        removeTouching(gin.class);
+        }
+    }
+    public void eatmang(){
+        if(isTouching(mang.class)){
+        removeTouching(mang.class);   
+        }
+    }
+    public void eatbom(){
+        if(isTouching(bom.class)){
+        removeTouching(bom.class);
+    }
+  }
+}
+
+
+    
